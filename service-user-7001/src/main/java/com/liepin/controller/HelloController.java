b21dab0014323552fdb907549395d64a686c9719
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-11-20 09:33:04 +0800

    1、lombok的使用 2、@Slf4j的日志输出使用，配置日志等级 debug 运行信息 info 程序输出的信息

--- service-user-7001/src/main/java/com/liepin/controller/HelloController.java
+++ service-user-7001/src/main/java/com/liepin/controller/HelloController.java
@@ -1,7 +1,7 @@
 package com.liepin.controller;
 
+import com.imooc.pojo.test.Stu;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +13,15 @@ public class HelloController {
 
     @GetMapping("hello")
     public Object hello() {
+
+        Stu stu = new Stu(1001, "imooc", 18);
+
+//        System.out.println(stu.toString());
+        log.info("info：" + stu.toString());
+        log.debug("debug：" + stu.toString());
+        log.warn("warn：" + stu.toString());
+        log.error("error：{}", stu.toString());
+
         return "Hello UserService~~~";
     }
 
