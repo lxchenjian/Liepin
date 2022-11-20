90d1f751d1a50cb48937c18864d91d0f577e4801
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-11-20 09:46:41 +0800

    如何进行优雅的Restful响应封装？ 1、打包成response的结构 2、生成响应生成工厂，按照枚举生成response 3、在枚举中完善所有的响应方式

--- service-user-7001/src/main/java/com/liepin/controller/HelloController.java
+++ service-user-7001/src/main/java/com/liepin/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.liepin.controller;
 
+import com.imooc.grace.result.GraceJSONResult;
+import com.imooc.grace.result.IMOOCJSONResult;
+import com.imooc.grace.result.ResponseStatusEnum;
 import com.imooc.pojo.test.Stu;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +26,28 @@ public class HelloController {
         log.error("error：{}", stu.toString());
 
         return "Hello UserService~~~";
+    }
+
+    @GetMapping("hello2")
+    public IMOOCJSONResult hello2() {
+        Stu stu = new Stu(1001, "imooc", 18);
+
+        return IMOOCJSONResult.ok(stu);
+//        return IMOOCJSONResult.ok("添加成功！");
+//        return IMOOCJSONResult.errorMsg("修改出错，请联系管理员");
+//        return IMOOCJSONResult.errorMap(map)
+//        return IMOOCJSONResult.errorUserTicket("用户会话校验失败！");
+    }
+
+    @GetMapping("hello3")
+    public GraceJSONResult hello3() {
+        Stu stu = new Stu(1001, "imooc", 18);
+
+//        return GraceJSONResult.ok(stu);
+//        return IMOOCJSONResult.ok("添加成功！");
+//        return IMOOCJSONResult.errorMsg("修改出错，请联系管理员");
+//        return IMOOCJSONResult.errorMap(map)
+        return GraceJSONResult.errorCustom(ResponseStatusEnum.SYSTEM_IO);
     }
 
 }
