c3a1ac8d5369e6d5b7add006eb788559e8363c22
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-11-20 09:05:32 +0800

    搭建了简单的服务器后端，测试hello接口

---
+++ service-user-7001/src/main/java/com/liepin/controller/HelloController.java
@@ -0,0 +1,19 @@
+package com.liepin.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("u")
+@Slf4j
+public class HelloController {
+
+    @GetMapping("hello")
+    public Object hello() {
+        return "Hello UserService~~~";
+    }
+
+}
