10dd57dd49e160684e9220dedcbe165be0a4294a
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-12-02 08:22:32 +0800

    1、实现验证码，并保存redirs

---
+++ Liepin-common/src/main/java/com/liepin/exceptions/GraceException.java
@@ -0,0 +1,14 @@
+package com.liepin.exceptions;
+
+import com.liepin.grace.result.ResponseStatusEnum;
+
+/**
+ * 优雅的处理异常，统一进行封装
+ */
+public class GraceException {
+
+    public static void display(ResponseStatusEnum statusEnum) {
+        throw new MyCustomException(statusEnum);
+    }
+
+}
