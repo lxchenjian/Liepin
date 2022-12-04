af4ad494691fd649f6582ff6ba3dec9c3b1af99f
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-12-04 23:49:28 +0800

    拦截请求的包装

--- gateway-8000/src/main/java/com/liepin/filter/SecurityFilterJWT.java
+++ gateway-8000/src/main/java/com/liepin/filter/SecurityFilterJWT.java
@@ -2,6 +2,9 @@ package com.liepin.filter;
 
 import com.google.gson.Gson;
 import com.liepin.base.BaseInfoProperties;
+import com.liepin.exceptions.GraceException;
+import com.liepin.grace.result.GraceJSONResult;
+import com.liepin.grace.result.ResponseStatusEnum;
 import com.liepin.utils.JWTUtils;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
@@ -60,10 +63,69 @@ public class SecurityFilterJWT extends BaseInfoProperties implements GlobalFilte
         }
         //log.info("被拦截了。。。")
 
-        // 放行
-        return chain.filter(exchange);
+        // 不放行，token校验在jwt校验的自身代码逻辑中，到达此处表示都是漏掉的可能没有配置在excludeList
+//        GraceException.display(ResponseStatusEnum.UN_LOGIN);
+//        return chain.filter(exchange);
+
+        return renderErrorMsg(exchange, ResponseStatusEnum.UN_LOGIN);
+
     }
 
+    public Mono<Void> dealJWT(String jwt, ServerWebExchange exchange, GatewayFilterChain chain, String key) {
+        try {
+            String userJson = jwtUtils.checkJWT(jwt);
+            ServerWebExchange serverWebExchange = setNewHeader(exchange, key, userJson);
+            return chain.filter(serverWebExchange);
+        } catch (ExpiredJwtException e) {
+            e.printStackTrace();
+            return renderErrorMsg(exchange, ResponseStatusEnum.JWT_EXPIRE_ERROR);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return renderErrorMsg(exchange, ResponseStatusEnum.JWT_SIGNATURE_ERROR);
+        }
+    }
+
+    /**
+     * 重新包装并且返回错误信息
+     * @param exchange
+     * @param statusEnum
+     * @return
+     */
+    public Mono<Void> renderErrorMsg(ServerWebExchange exchange,
+                                     ResponseStatusEnum statusEnum) {
+        // 1. 获得response
+        ServerHttpResponse response = exchange.getResponse();
+
+        // 2. 构建jsonResult
+        GraceJSONResult jsonResult = GraceJSONResult.exception(statusEnum);
+
+        // 3. 修改response的code为500
+        response.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+
+        // 4. 设定header类型
+        if (!response.getHeaders().containsKey("Content-Type"))
+            response.getHeaders().add("Content-Type", MimeTypeUtils.APPLICATION_JSON_VALUE);
+
+        // 5. 转换json并且向response中写入数据
+        String resultJson = new Gson().toJson(jsonResult);
+        DataBuffer dataBuffer = response
+                .bufferFactory()
+                .wrap(resultJson.getBytes(StandardCharsets.UTF_8));
+        return response.writeWith(Mono.just(dataBuffer));
+    }
+
+    public ServerWebExchange setNewHeader(ServerWebExchange exchange,
+                                          String headerKey,
+                                          String headerValue) {
+        // 重新构建新的request
+        ServerHttpRequest newRequest = exchange.getRequest()
+                .mutate()
+                .header(headerKey, headerValue)
+                .build();
+        // 替换原来的request
+        ServerWebExchange newExchange = exchange.mutate().request(newRequest).build();
+        return newExchange;
+    }
 
 
     // 过滤器的顺序，数字越小优先级则越大
