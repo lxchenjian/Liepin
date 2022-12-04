43e4fb1c7192b7e684f3f624f800cfcbd1729fae
陈健 <5414779+chen_jian_xiangxi@user.noreply.gitee.com>
2022-12-04 17:27:48 +0800

    网关定义哪些接口不需要拦截

---
+++ gateway-8000/src/main/java/com/liepin/filter/SecurityFilterJWT.java
@@ -0,0 +1,74 @@
+package com.liepin.filter;
+
+import com.google.gson.Gson;
+import com.liepin.base.BaseInfoProperties;
+import com.liepin.utils.JWTUtils;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.MimeTypeUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Component
+@Slf4j
+public class SecurityFilterJWT extends BaseInfoProperties implements GlobalFilter, Ordered {
+
+    public static final String HEADER_USER_TOKEN = "headerUserToken";
+
+    @Autowired
+    private ExcludeUrlProperties excludeUrlProperties;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    // 路径匹配的规则器
+    private AntPathMatcher antPathMatcher = new AntPathMatcher();
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        // 1. 获取当前的请求路径
+        String url = exchange.getRequest().getURI().getPath();
+        log.info("SecurityFilterJWT url=" + url);
+
+        // 2. 获得所有的需要排除校验的url list
+        List<String> excludeList = excludeUrlProperties.getUrls();
+
+        // 3. 校验并且排除excludeList
+        if (excludeList != null && !excludeList.isEmpty()) {
+            for (String excludeUrl : excludeList) {
+                if (antPathMatcher.matchStart(excludeUrl, url)) {
+                    // 如果匹配到，则直接放行，表示当前的请求url是不需要被拦截校验的
+                    return chain.filter(exchange);
+                }
+            }
+        }
+        //log.info("被拦截了。。。")
+
+        // 放行
+        return chain.filter(exchange);
+    }
+
+
+
+    // 过滤器的顺序，数字越小优先级则越大
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
